387bc613baf21d10b231577421da7e967e18f069
tot005 <tot005@ifilab85-lnx.stud.cs.uit.no>
2019-11-24 22:58:56 +0100

    222

--- main.cpp
+++ main.cpp
@@ -10,6 +10,7 @@
 #include <upcxx/upcxx.hpp>
 #include "eigen/Eigen/Dense"
 
+
 #include <sys/time.h>
 #include <sys/resource.h>
 
@@ -42,11 +43,11 @@ struct matrix_array
     int index;
     double *nested_array;
 };
-struct rating_data
-{
-    double rating;
-    double item;
-};
+// struct rating_data
+// {
+//     double rating;
+//     double item;
+// };
 struct starting_queue_range
 {
     int start;
@@ -55,50 +56,6 @@ struct starting_queue_range
 
 
 
-
-// matrix_array *importData(const char *filename,int n_items,int n_users,matrix_array *new_array){
-//     string delimiter = " ";
-
-//     ifstream data;
-//     data.open(filename);
-
-//     string row;
-
-//     // MatrixXd training_set = MatrixXd::Zero(n_users, n_items);
-    
-//     // Resets getline function to start of file
-//     data.clear();
-//     data.seekg(0, ios::beg);
-//     int x = 0;
-
-//     // Adds all rating to the matrix using user and items in a row as index
-//     while(getline(data, row)){
-//         string str_tmp = row.substr(0, row.find(delimiter));
-//         int user_idx = stoi(str_tmp);
-//         cout << "user_idx " << user_idx << endl;
-
-//         int len = str_tmp.length() + 1;
-//         cout << "len " << len << endl;
-//         str_tmp = row.substr(len, row.find(delimiter, len));
-//         int item_idx = stoi(str_tmp);
-//         cout << "item_ idx " << item_idx << endl;
-
-        
-//         str_tmp = row.substr(row.find(delimiter, len), row.find("\n"));
-//         float rating = stof(str_tmp);
-//         cout << "str_tmp " << str_tmp << " and rating " << rating << endl;
-        
-
-//         // training_set(user_idx - 1, item_idx - 1) = rating;
-//   //
-//   //    må legge til et system for å få all dataen på --rating
-//   //
-
-//         // new_array[user_idx - 1].array[item_idx-1] = rating;
-//     }
-
-//     return (matrix_array*)new_array;
-// }
 
 
 
@@ -200,14 +157,29 @@ int main(int argc, char *argv[]){
     cout << argc << endl;
     cout << argv[0] << endl;
 
-    
+
+
+    // float stepsize;
+    const int k = 100;
+    const float alpha = 0.012;
+    const float beta = 0.01;
+    const float lambda = 0.05;
+    float t = 5;
+
+
     matrix_size training_matrix_size = get_matrix_size(training_matrix_size);
+    int n_users = training_matrix_size.users;
+    int n_items = training_matrix_size.items;
+
+    multimap<int, int> items;
+    multimap<pair <int, int> , float> rating_map;
+
 
     cout << "amount of items in matrix" << training_matrix_size.items << endl;
     cout << "amount of users in matrix" << training_matrix_size.users << endl;
 
     std::default_random_engine generator;
-    uniform_real_distribution<double> distribution(0.0,1.0);
+    uniform_real_distribution<double> distribution(0.0,0.1);
     // uniform_real_distribution
     // uniform_int_distribution
 
@@ -219,11 +191,8 @@ int main(int argc, char *argv[]){
         // malloc(sizeof(double)*training_matrix_size.items)
         training_matrix[i].nested_array = new double[training_matrix_size.items];
     }
-    // *training_matrix = importData("Data_for_SGD/train_small.txt",training_matrix_size.items,training_matrix_size.users,training_matrix);
-    // // user 1 item 1
-    // cout << training_set(0, 0) << endl;
-    // // user 1 item 1029
-    // cout << training_set(0, 1028) << endl;
+
+
     string delimiter = " ";
 
     ifstream data;
@@ -253,73 +222,64 @@ int main(int argc, char *argv[]){
 
         
         str_tmp = row.substr(row.find(delimiter, len), row.find("\n"));
-        float rating = stof(str_tmp);
-            // cout << "str_tmp " << str_tmp << " and rating " << rating << endl;
-            // cout << "item_ idx " << item_idx << " user_idx " << user_idx << endl;
-            // cout << " row " << row << endl;
-        // cout << "array size  " << sizeof(training_matrix[user_idx -1].array) << endl;
-        // int size = sizeof(training_matrix[user_idx -1].array[])
-        //  cout << " previos rating " << training_matrix[user_idx - 1].nested_array[item_idx-1]
-        training_matrix[user_idx - 1].nested_array[item_idx-1] = rating;
+        float rating_from_data = stof(str_tmp);
+        
+        items.insert({item_idx,user_idx});
+ 
+        rating_map.insert({{user_idx, item_idx},rating_from_data});
+
     }
 
-    cout << "making W and H matrix !!!!!!!" << endl;
+    upcxx::init(); ////////////////////////////////////////////////////////////////////////////////
 
-
-    matrix_array *W_matrix = new matrix_array[training_matrix_size.items];
-    // // user 11 item 88
-    for(int i = 0; i < training_matrix_size.items; i++)
+    upcxx::dist_object<upcxx::global_ptr<queue<int>>> item_queue(upcxx::new_ <queue<int>>());
+    upcxx::dist_object<upcxx::global_ptr<double>> H_array(upcxx::new_array<double>((k * n_items)/upcxx::rank_n()));
+    double *W_array = new double[k * n_users];
+    for(int i = 0; i < n_users*k; i++)
     {
-        W_matrix[i].index = i;
-        // malloc(sizeof(double)*training_matrix_size.items)
-        W_matrix[i].nested_array = new double[2];
-        // cout << " i " << i << endl;
-        for(int j = 0; j < 2; j++)
-        {
-            // cout << " j " << j << endl;
-            // float temp = (float)1/sqrt((int)training_matrix_size.items);
-            // cout << "value at index " << W_matrix[i].nested_array[j] << endl;
-            // cout << " temp " << temp << endl;
-            // W_matrix[i].nested_array[j] = (double)((float)rand() % temp);
-            W_matrix[i].nested_array[j] = distribution(generator);
-        }
+        W_array[i] = distribution(generator);
     }
-    cout << " laget W_matrix" << endl;
-
-    matrix_array *H_matrix = new matrix_array[2];
-
-    for(int i = 0; i < 2; i++)
+    for(int i = 0; i < (k * n_items)/upcxx::rank_n(); i++)
     {
-        H_matrix[i].index = i;
-        // malloc(sizeof(double)*training_matrix_size.items)
-        H_matrix[i].nested_array = new double[training_matrix_size.users];
-        for(int j = 0; j < training_matrix_size.users; j++)
-        {
-            // int temp = (int)1/sqrt((int)training_matrix_size.items);
-            // H_matrix[i].nested_array[j] = (double)(rand() % temp);
-            H_matrix[i].nested_array[j] = distribution(generator);
-        }
+        H_array->local()[i] = (double)distribution(generator);
     }
 
-    cout << " laget H_matrix" << endl;
-
-    //create W and H matrix
-
-    cout << "starting upcxx !!!!!!!" << endl;
-    upcxx::init();
-    upcxx::dist_object<upcxx::global_ptr<queue<matrix_array>>> worker_queues(upcxx::new_ <queue<matrix_array>>());
-    starting_queue_range starting_range = get_range(upcxx::rank_me(),training_matrix_size.users);
+    starting_queue_range starting_range = get_range(upcxx::rank_me(),n_users);
     while (starting_range.start < starting_range.end)
     {
-        worker_queues->local()->push(training_matrix[starting_range.start]);
+        item_queue->local()->push(starting_range.start);
         starting_range.start++;
     }
     
 
-    while(worker_queues->local()->empty() != true)
+    while(item_queue->local()->empty() != true)
     {
-        matrix_array* current_array = worker_queues->local()->pop();
+        int j = item_queue->local()->front();
+        item_queue->local()->pop();
         
+        // int user_index = current_array.index;
+
+        auto item_index = items.find(j);
+
+        float current_error = 0;
+        for(int i = 0; i < k; i++)
+        {
+            cout << "W_array[i] " << W_array[i] << "H_array->local()[current_item] " << H_array->local()[j] << endl;
+            current_error += (W_array[i] * H_array->local()[j]);
+        }
+        //calculate stepsize
+        float S_t = alpha / (1 + beta * t);
+        t*=1.1;
+
+
+
+
+        cout << "current error " << current_error << endl;
+
+
+        
+    
+
     }
 
     //splitte mellom worker queue's
@@ -337,5 +297,8 @@ int main(int argc, char *argv[]){
 
     cout << "Hei" << endl;
     upcxx::finalize();
+
+    //calc rmse
+
     return 0;
 }
